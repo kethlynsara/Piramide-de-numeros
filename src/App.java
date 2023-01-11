828b2abd21af88ffa3c00485f29dc7684327ba9d
Kethlyn <kethlynsaraa@gmail.com>
2023-01-11 15:21:49 -0300

    feat: get keyboard input

--- src/App.java
+++ src/App.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        int qtd = 5;
-        for (int i = 1; i <= qtd; i++) {
+        Scanner n = new Scanner(System.in);
+        
+        System.out.printf("Informe o número de início do intervalo:\n");
+        int qtd1 = n.nextInt();
+
+        System.out.printf("Informe o número final do intervalo:\n");
+        int qtd2 = n.nextInt();
+
+        for (int i = qtd1; i <= qtd2; i++) {
             String word = "";
-            for (int j = 1; j <= i; j++) {
+            for (int j = qtd1; j <= i; j++) {
                 word += i;
             }
             System.out.println(word);
